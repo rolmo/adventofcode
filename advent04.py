4ae20b4e2af79a350a51b89bc9c53dc8e810c859
Roland Mohrbacher <roland.mohrbacher@axelspringer.de>
2021-12-04 17:19:38 +0100

    Add decription of the puzzle

--- advent04.py
+++ advent04.py
@@ -2,11 +2,31 @@
 
 # Usage: cat advent04.input | ./advent04.py
 
+"""
+Part One:
+
+Bingo is played on a set of boards each consisting of a 5x5 grid of numbers.
+Numbers are chosen at random, and the chosen number is marked on all boards on
+which it appears. (Numbers may not appear on all boards.) If all numbers in any
+row or any column of a board are marked, that board wins. (Diagonals don't count.)
+
+The score of the winning board can now be calculated. Start by finding the sum
+of all unmarked numbers on that board.
+Then, multiply that sum by the number that was just called when the board won.
+
+Part Two:
+
+Figure out which board will win last
+
+Calculate the result as in part one.
+"""
+
+
 import sys
 import re
 
 def main():
-    # First line of input contains the guessed numbers (we convert to array if int)
+    # First line of input contains the guessed numbers (we convert to array-of-int)
     guessed_numbers=list(map(int, sys.stdin.readline().strip().split(',')))
 
     # Array with all board:
@@ -15,7 +35,7 @@ def main():
     # Read input and create all boards:
     for line in sys.stdin:
         if line.strip():
-            # Non empty line: convert to array of int and add to the last created board:
+            # Non empty line: convert to array-of-int and add to the last board:
             board.add_row(list(map(int, re.split("\s+",line.strip()))))
         else:
             # Empty line: create a new board
@@ -55,19 +75,19 @@ def guess (boards, guessed_numbers, strategy):
 class Board:
 
     def __init__ (self):
-        self.rows = []
+        self.board = []
         self.marked = []
         for row in range(5):
             self.marked.append([False]*5)
         self.bingo = False
 
     def add_row (self, row):
-        self.rows.append(row)
+        self.board.append(row)
 
     def mark (self, guessed):
         for row in range(5):
             for col in range(5):
-                if self.rows[row][col] == guessed:
+                if self.board[row][col] == guessed:
                     #print("Mark number {} row={} and col={}".format(guessed,row,col))
                     self.marked[row][col] = True
 
@@ -95,13 +115,13 @@ class Board:
         for row in range(5):
             for col in range(5):
                 if not self.marked[row][col]:
-                    sum += self.rows[row][col]
+                    sum += self.board[row][col]
         return sum
 
     # Only for the debug output: string representation of the board and marks
     def __str__(self):
         out = "-------------\n"
-        for row in self.rows:
+        for row in self.board:
             for num in row:
                 out += "|" + str(num)
             out += "|\n"
@@ -110,6 +130,8 @@ class Board:
                 out += "|" + ("X" if bool else "-")
             out += "|\n"
         return out
+
+
 
 if __name__ == '__main__':
     main()
