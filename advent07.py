30792a0f4f7904428c71d38e7d5ccc078c4bd4ed
Roland Mohrbacher <roland.mohrbacher@axelspringer.de>
2021-12-07 16:47:15 +0100

    optimize the order to find the best result faster

--- advent07.py
+++ advent07.py
@@ -6,44 +6,89 @@
 """
 https://adventofcode.com/2021/day/7
 
+
 Part1:
 
 Align all positions to the same position.
-Use as few fuel as possible
-Fuel consumption is linear to the distance
+Consume as little fuel as possible
+Fuel consumption is linear with distance
 
 Part2:
 
-Fuel consumption is gauss sum uf the distance
+Fuel consumption is the Gaussian sum of distance
 
 """
 
 import sys
+import time
+import statistics
 
 def main():
 
-    gauss_sum = lambda x: int(x*(x+1)/2)
 
     start_positions=list(map(int, sys.stdin.readline().strip().split(',')))
-    max_position = max(start_positions)
-    min_fuel_dist = min_fuel_gauss = None
-    for target_pos in range(max_position+1):
-        fuel_dist = fuel_gauss = 0
-        for pos in start_positions:
-            dist = abs(target_pos-pos)
-            fuel_dist  += dist
-            fuel_gauss += gauss_sum(dist)
-        if not min_fuel_dist or fuel_dist < min_fuel_dist:
-            best_position_dist = target_pos
-            min_fuel_dist = fuel_dist
-        if not min_fuel_gauss or fuel_gauss < min_fuel_gauss:
-            best_position_gauss = target_pos
-            min_fuel_gauss = fuel_gauss
 
-    print("Best target position (linear fuel consumption): {}; Fuel: {}".format(best_position_dist, min_fuel_dist))
-    # Best target position (linear fuel consumption): 317; Fuel: 331067
-    print("Best target position (gauss fuel consumption): {}; Fuel: {}".format(best_position_gauss, min_fuel_gauss))
-    # Best target position (gauss fuel consumption): 458; Fuel: 92881128
+    start_time = time.time()
+    (position, fuel) = linear_via_median(start_positions)
+    end_time = time.time()
+
+
+    print("Best target position (linear fuel consumption): {}; Fuel: {} (consumed time: {} ms)".format(position, fuel, 1000 * (end_time - start_time)))
+    # Best target position (linear fuel consumption): 317; Fuel: 331067 (consumed time: 0.25 ms)
+
+    start_time = time.time()
+    (position, fuel) = gauss_fuel_consumption(start_positions)
+    end_time = time.time()
+
+    print("Best target position (gauss fuel consumption): {}; Fuel: {} (consumed time: {} ms)".format(position, fuel, 1000 * (end_time - start_time)))
+    # Best target position (gauss fuel consumption): 458; Fuel: 92881128 (consumed time: 51.6 ms)
+
+
+def gauss_fuel_consumption (positions):
+
+    gauss_sum = lambda x: int(x*(x+1)/2)
+
+    # Strategie:
+    # We split the list at the median in two sub lists ("left_positions" (decreased order) and "right_positions" (increased order)).
+    # - We start with "left_positions" (median ... 0) and test each position as long as the fuel consumption falls.
+    # - We stop when the fuel consumtion raises
+    # - Then we loop over the list "right_positions" (median ... max) as long as we get lower fuel consumption
+    # - Again: we stop when the fuel consumtion raises
+
+    max_position = max(positions)
+    median = int(statistics.median(positions))
+    left_positions = list(range(median,0,-1))
+    right_positions = list(range(median,max_position+1))
+    min_fuel = None
+    for target_pos in left_positions:
+        fuel = 0
+        for pos in positions:
+            fuel += gauss_sum(abs(target_pos-pos))
+        if not min_fuel or fuel < min_fuel:
+            best_position = target_pos
+            min_fuel = fuel
+        if min_fuel != None and fuel > min_fuel:
+            break
+    for target_pos in right_positions:
+        fuel = 0
+        for pos in positions:
+            fuel += gauss_sum(abs(target_pos-pos))
+        if not min_fuel or fuel < min_fuel:
+            best_position = target_pos
+            min_fuel = fuel
+        if min_fuel != None and fuel > min_fuel:
+            break
+
+    return(best_position, min_fuel)
+
+
+def linear_via_median (positions):
+    median = int(statistics.median(positions))
+    fuel = 0
+    for pos in positions:
+        fuel += abs(median-pos)
+    return (median,fuel)
+
 
 if __name__ == '__main__':
     main()
