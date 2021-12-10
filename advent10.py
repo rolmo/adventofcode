c5c56d35a29b8eebb77cf22857226a9d17da83e2
Roland Mohrbacher <roland.mohrbacher@axelspringer.de>
2021-12-10 11:47:54 +0100

    Some cleanups

--- advent10.py
+++ advent10.py
@@ -13,11 +13,17 @@ import sys
 from dataclasses import dataclass
 import statistics
 
-Opener_sign =  {"<":True,"(":True,"{":True,"[":True}
-Closer_sign =  {">":True,")":True,"}":True,"]":True}
+
+# Set some constants:
 Close_sign_for = {"<":">","(":")","{":"}","[":"]"}
+# All Keys:
+Opener_sign = [*Close_sign_for]
+# All values:
+Closer_sign = list(Close_sign_for.values())
+
 Points_for_unexpected_closer =  {">":25137,")":3,"}":1197,"]":57}
 Points_for_autocomplete =  {">":4,")":1,"}":3,"]":2}
+
 
 
 def main():
@@ -29,16 +35,18 @@ def main():
         try:
             check_code(code)
         except UnexpectedCloser as e:
+            #print(e, e.char)
             error_points += Points_for_unexpected_closer[e.char]
         except CloserWithoutOpener as e:
+            #print(e, e.char)
             error_points += Points_for_unexpected_closer[e.char]
         except UnclosedOpener as e:
+            #print(e, e.chars)
             score = 0
             for char in e.chars[::-1]:
                 score *= 5
                 score += Points_for_autocomplete[Close_sign_for[char]]
             autocomplete_scores.append(score)
-        #except UnclosedOpener:
 
     print("Corrupted Code Points:", error_points)       # 311949
     print("Autocomplete Points:", statistics.median(autocomplete_scores))  # 3042730309
@@ -47,7 +55,6 @@ def main():
 def check_code(code):
     stack = []
     for c in code:
-        # print(stack)
         if c in Opener_sign:
             stack.append(c)
         elif c in Closer_sign:
@@ -64,30 +71,26 @@ def check_code(code):
 
 
 
-class Error(Exception):
-    """Base class for other exceptions"""
-    pass
-
-class UnexpectedCloser(Error):
-    """Raised when we get an closer sign that matches not the opener"""
+class UnexpectedCloser(Exception):
+    """Raised when we find a closer that matches not the corresponding opener"""
 
     def __init__(self, char, message="Unexpected closer"):
         self.char = char
         self.message = message
         super().__init__(self.message)
 
-class CloserWithoutOpener(Error):
-    """Raised when we get an closer but there is no open opener"""
+class CloserWithoutOpener(Exception):
+    """Raised when we find a closer but there is no open opener"""
 
     def __init__(self, char, message="Closer without opener"):
         self.char = char
         self.message = message
         super().__init__(self.message)
 
-class UnclosedOpener(Error):
-    """We ar at the end of the strting, but there are unclosed openers"""
+class UnclosedOpener(Exception):
+    """We ar at the end of the string, but there are unclosed openers"""
 
-    def __init__(self, chars, message="Closer without opener"):
+    def __init__(self, chars, message="Opener without closer"):
         self.chars = chars
         self.message = message
         super().__init__(self.message)
