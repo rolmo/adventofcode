34caf0226cb0378e0ec6505b26934cba4f005933
Roland Mohrbacher <roland.mohrbacher@axelspringer.de>
2022-12-03 20:09:01 +0100

    use own object for each item

--- advent02.py
+++ advent02.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python3
 
-# Usage: cat advent01.input | ./advent01_1.py
-
 """
 https://adventofcode.com/2022/day/2
 
@@ -11,31 +9,6 @@ Usage: cat advent02.input | ./advent02.py
 
 import sys
 
-Char2item = {
-    "A": "Rock",
-    "B": "Paper",
-    "C": "Scissors",
-    "X": "Rock",
-    "Y": "Paper",
-    "Z": "Scissors"        
-}
-Worser_as = {
-    "Rock": "Scissors",
-    "Scissors": "Paper",
-    "Paper": "Rock"
-}
-Better_as = {
-    "Scissors": "Rock",
-    "Paper": "Scissors",
-    "Rock": "Paper" 
-}
-Points4item = {
-    "Rock": 1,
-    "Paper": 2,
-    "Scissors": 3
-}
-
-
 def main ():
 
     my_points1 = 0  # Part One
@@ -43,21 +16,23 @@ def main ():
     for line in sys.stdin:
         (left_char,right_char) = line.split()
 
-        # Part One
-        round = Round(Char2item[left_char],Char2item[right_char])
+        round = Round(get_item(left_char), get_item(right_char))
         my_points1 += round.points_right()
-        
+
         # Part Two
         if right_char == "X":
-            # X=must loose: worser item for me
-            round2 = Round(Char2item[left_char], Worser_as[Char2item[left_char]])
+            # X = must loose: worser item for me
+            left = get_item(left_char)
+            right = get_item(left.worser)
         if right_char == "Y":
-            # Y=draw: same item for both sides
-            round2 = Round(Char2item[left_char], Char2item[left_char])
+            # Y = draw: same item for both sides
+            left = right = get_item(left_char)
         if right_char == "Z":
-            # Z=must win: better item for me
-            round2 = Round(Char2item[left_char], Better_as[Char2item[left_char]])
-        my_points2 += round2.points_right()
+            # Z = must win: better item for me
+            left = get_item(left_char)
+            right = get_item(left.better)
+        round = Round(left, right)
+        my_points2 += round.points_right()
 
     print("My points (Part 1):", my_points1)
     # My points (Part 1): 13221
@@ -66,22 +41,31 @@ def main ():
     # My points (Part 2): 13131
 
 
+
+def get_item (char: str) -> object:
+    match char:
+        case "A" | "X" | "Rock":
+            return Rock()
+        case "B" | "Y" | "Paper":
+            return Paper()
+        case "C" | "Z" | "Scissors":
+            return Scissors()
+
+
+
 class Round:
 
-    left = None   # left is for opponent
-    right = None  # right is for me
-
     def __init__ (self, left, right) -> None:
-        self.left = Item(left)
-        self.right = Item(right)
+        self.left = left
+        self.right = right
     
     def points_right (self) -> int:
         win_points = 0
         if self.left == self.right:
             win_points = 3
-        if self.right.better(self.left):
+        if self.right > self.left:
             win_points = 6
-        return win_points + self.right.points()
+        return win_points + self.right
 
     def __str__ (self) -> str:
         return "{} - {} (right gets {} points)".format(self.left, self.right, self.points_right())
@@ -90,25 +74,37 @@ class Round:
 
 class Item:
 
-    def __init__ (self, item) -> None:
-        self.item = item
+    def __str__(self) -> str:
+        return type(self).__name__
 
-    def better (self, opponent) -> bool:
-        if Worser_as[self.item] == opponent.item:
+    def __eq__(self, other: object) -> bool:
+        if type(self).__name__ == type(other).__name__:
             return True
         return False
 
-    def points (self) -> int:
-        return Points4item[self.item]
-
-    def __eq__(self, opponent: object) -> bool:
-        if self.item == opponent.item:
+    def __gt__(self, other: object) -> bool:
+        if type(other).__name__ == self.worser:
             return True
         return False
 
-    def __str__ (self) -> str:
-        return self.item
+    def __radd__(self, other):
+        return self.points + other
 
+
+class Rock(Item):
+    better = "Paper"
+    worser = "Scissors"
+    points = 1
+
+class Paper(Item):
+    better = "Scissors"
+    worser = "Rock"
+    points = 2
+
+class Scissors(Item):
+    better = "Rock"
+    worser = "Paper"
+    points = 3
 
 
 if __name__ == '__main__':
