80732fa28a678601aec63966887cf6f72df079fe
Roland Mohrbacher <roland.mohrbacher@axelspringer.de>
2022-12-06 20:08:11 +0100

    DRY

--- advent06.py
+++ advent06.py
@@ -10,17 +10,17 @@ Usage: cat advent06.input | python3 ./advent06.py
 START_OF_PACKET_MARKER_LENGTH = 4
 START_OF_MESSAGE_MARKER_LENGTH = 14
 
-
 import sys
 
-input = sys.stdin.read().strip()
+def main ():
+    input = sys.stdin.read().strip()
+    print (find_uniq_marker(START_OF_PACKET_MARKER_LENGTH, input))
+    print (find_uniq_marker(START_OF_MESSAGE_MARKER_LENGTH, input))
 
-for n in range(len(input)):
-    if len(set(input[n:n+START_OF_PACKET_MARKER_LENGTH])) == START_OF_PACKET_MARKER_LENGTH:
-        print("START_OF_PACKET_MARKER:", n+START_OF_PACKET_MARKER_LENGTH, input[n:n+START_OF_PACKET_MARKER_LENGTH])
-        break
+def find_uniq_marker (length, input):
+    for n in range(len(input) - length):
+        if len(set(input[n:n+length])) == length:
+            return n+length
 
-for n in range(len(input)):
-    if len(set(input[n:n+START_OF_MESSAGE_MARKER_LENGTH])) == START_OF_MESSAGE_MARKER_LENGTH:
-        print("START_OF_MESSAGE_MARKER_LENGTH:", n+START_OF_MESSAGE_MARKER_LENGTH, input[n:n+START_OF_MESSAGE_MARKER_LENGTH])
-        break
+if __name__ == '__main__':
+    main()
