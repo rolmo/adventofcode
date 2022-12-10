ccc9993b804b237a8ff37123e3875e0d3965c5e4
Roland Mohrbacher <roland.mohrbacher@axelspringer.de>
2022-12-10 14:59:25 +0100

    day 10

---
+++ advent10.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+
+"""
+https://adventofcode.com/2022/day/10
+
+Usage: cat advent10.input | python3 ./advent10.py
+
+"""
+import sys
+
+ROPE_LEN = 2  # 2
+
+def main ():
+    input = sys.stdin.read().strip().split("\n")
+
+    next_value = 1
+    register = []
+    for command in input:
+        if command == "noop":
+            register.append(next_value)
+        else:
+            (op,arg) = command.split(" ")
+            register.append(next_value)
+            register.append(next_value)
+            next_value += int(arg)
+            
+    # Part1:
+    sum_signal_strength = 0
+    for cycle in range(20,221,40):
+        sum_signal_strength += cycle * register[cycle - 1]
+
+    print("Sum of signal strength:", sum_signal_strength)
+    # Sum of signal strength: 12560
+
+    # Part2:
+    for row in range(6):
+        for col in range(40):
+            x = register[row*40+col]
+            if abs(col - x) <= 1:
+                print("#", end="")
+            else:
+                print(" ", end="")
+        print("")
+    # Output:
+    # PLPAFBCL
+
+
+if __name__ == '__main__':
+    main()
